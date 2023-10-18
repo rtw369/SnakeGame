2ec8ccc34cf78a2289263a287a139d504da6561a
Shawn Rim <taewoorim@gmail.com>
2023-10-17 22:35:36 -0700

    Updated main.py file
    Initialized basic pygame window structure.

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import pygame
 
+pygame.init()
+
+FPS = 60
+WIDTH, HEIGHT = 800, 800
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Snake Game")
+
 
 def game():
-    return
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
 
 
 if __name__ == "__main__":
